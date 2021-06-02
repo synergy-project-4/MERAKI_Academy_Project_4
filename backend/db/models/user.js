f4b0990e5c4ece4e5fae7dbc5324e09edcf14aca
TariqBazadough <tariq.bazadough@gmail.com>
2021-06-02 21:46:03 +0300

    pull update

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 // create new user
 const users = new mongoose.Schema({
