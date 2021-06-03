d0f364cf1f143bb25c8253b9ac1c43f027600aeb
TariqBazadough <tariq.bazadough@gmail.com>
2021-06-03 18:25:24 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db/db");
 const userRouter = require("./routers/routes/auth/signUp");
-const {productRouter} = require("./routers/routes/product")
+const productRouter = require("./routers/routes/product");
 
 const app = express();
 
@@ -16,8 +16,7 @@ app.use(cors());
 
 //app routers
 app.use(userRouter);
-app.use(productRouter)
-
+app.use(productRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
