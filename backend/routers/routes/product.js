df3aea912755e0f6839c3958b66ea0743f1089c7
arefallabadi <labadi.aref@gmail.com>
2021-06-03 18:36:18 +0300

    done with cartSchema, send to cart function and show cart function

--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -1,30 +1,23 @@
 const express = require("express");
 
-
-
-
-
-const { createProduct ,
-    deleteProduct,
-	updateProduct,getProductToHistory,getAllProducts,pendingApproval ,manageProduct } = require('../controllers/product');
-
+const {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  getProductToHistory,
+  getAllProducts,
+  pendingApproval,
+  manageProduct,
+} = require("../controllers/product");
 
 const productRouter = express.Router();
 
-
-productRouter.post('/create/product', createProduct);
-productRouter.get('/products/approval', pendingApproval);
-productRouter.get('/manage/product', manageProduct);
-productRouter.put('/manage/prodcut',updateProduct);
-productRouter.delete('/manage/prodcut',deleteProduct);
+productRouter.post("/create/product", createProduct);
+productRouter.get("/products/approval", pendingApproval);
+productRouter.get("/manage/product", manageProduct);
+productRouter.put("/manage/prodcut", updateProduct);
+productRouter.delete("/manage/prodcut", deleteProduct);
 productRouter.get("/product/history", getProductToHistory);
 productRouter.get("/home", getAllProducts);
 
-module.exports = productRouter
-
-
-
-
-
-
-
+module.exports = productRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db/db");
 const userRouter = require("./routers/routes/auth/signUp");
-const {productRouter} = require("./routers/routes/product")
+const productRouter = require("./routers/routes/product");
+const cartRouter = require("./routers/routes/cart");
 
 const app = express();
 
@@ -16,8 +17,8 @@ app.use(cors());
 
 //app routers
 app.use(userRouter);
-app.use(productRouter)
-
+app.use(productRouter);
+app.use(cartRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
