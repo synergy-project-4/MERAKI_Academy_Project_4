ae13c29f05eb4659858573c0dc29da7aac692db9
Your Name <you@example.com>
2021-06-03 11:16:13 +0300

    Done with admin approvel

--- backend/routers/controllers/product.js
+++ backend/routers/controllers/product.js
@@ -1,7 +1,7 @@
 const productsModel = require('./../../db/models/productSchema');
 const createProduct = (req, res) => {
-	const { title, tags,description, price, quantity, optionsToExchange, itemLength, itemHeight,itemWidth, itemWeight,
-		location, shortDescription,userId } = req.body;
+	const { title, tags, description, price, quantity, optionsToExchange, itemLength, itemHeight, itemWidth, itemWeight,
+		location, shortDescription, userId } = req.body;
 
 	const product = new productsModel({
 		title,
@@ -29,6 +29,33 @@ const createProduct = (req, res) => {
 			res.status(404).send(err);
 		});
 };
+
+const pendingApproval = (req, res) => {
+	productsModel
+		.find({ ready: false })
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+};
+
+const manageProduct = (req, res) => {
+	const id = req.query.id
+	productsModel
+		.find({ ready: false, userId: id })
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+};
+
+
 module.exports = {
-	createProduct
+	createProduct,
+	pendingApproval,
+	manageProduct
 };
--- backend/routers/routes/product.js
+++ backend/routers/routes/product.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
-const { createProduct} = require('../controllers/product');
+const { createProduct ,pendingApproval ,manageProduct} = require('../controllers/product');
 
 const productRouter = express.Router();
 
 productRouter.post('/create/product', createProduct);
+productRouter.get('/products/approval', pendingApproval);
+productRouter.get('/manage/product', manageProduct);
+
 
 module.exports = productRouter;
