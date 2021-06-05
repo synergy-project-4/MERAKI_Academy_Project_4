333e5bef852de5939670f322aeb1109e4edd269f
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-06-05 16:35:05 +0300

    login

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Register from "../auth/signUp/index";
+
 
 const Navigation = () => {
   return (
     <div className="App">
-      <Link to="/register">Register</Link>
+      <Link to="/login">login</Link>
     </div>
   );
 };
