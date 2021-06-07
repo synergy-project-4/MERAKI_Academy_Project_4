1e206f92fb2694149eebe14d51a73a0d852261e2
TariqBazadough <tariq.bazadough@gmail.com>
2021-06-07 09:14:34 +0300

    fixed sign-up functionality , and added css for sing-in and sign-up pages

--- frontend/src/components/auth/signUp/index.js
+++ frontend/src/components/auth/signUp/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { RegisterContext } from "../../../contexts/register";
+import "./signUp.css";
 
 const Register = () => {
   const registerContext = useContext(RegisterContext);
@@ -11,53 +12,70 @@ const Register = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="First Name"
-          onChange={(e) => {
-            registerContext.setFirstName(e.target.value);
-          }}
-        ></input>
-        <input
-          type="text"
-          placeholder="Last Name"
-          onChange={(e) => {
-            registerContext.setLastName(e.target.value);
-          }}
-        ></input>
-        <input
-          type="text"
-          placeholder="City"
-          onChange={(e) => {
-            registerContext.setCity(e.target.value);
-          }}
-        ></input>
-        <input
-          type="email"
-          placeholder="E-mail"
-          onChange={(e) => {
-            registerContext.setEmail(e.target.value);
-          }}
-        ></input>
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => {
-            registerContext.setPassword(e.target.value);
-          }}
-        ></input>
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          onChange={(e) => {
-            registerContext.setConfirmPassword(e.target.value);
-          }}
-        ></input>
-        <button>Sign-Up</button>
-      </form>
+      <div className="sign-up-body">
+        <div className="sign-up">
+          <p>WebSite Name</p>
+          <p>Sign-up</p>
+          <form onSubmit={handleSubmit} className="sign-up-form">
+            <input
+              className="input"
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => {
+                registerContext.setFirstName(e.target.value);
+              }}
+            ></input>
+            <input
+              className="input"
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => {
+                registerContext.setLastName(e.target.value);
+              }}
+            ></input>
+            <input
+              className="input"
+              type="text"
+              placeholder="City"
+              onChange={(e) => {
+                registerContext.setCity(e.target.value);
+              }}
+            ></input>
+            <input
+              className="input"
+              type="email"
+              placeholder="E-mail"
+              onChange={(e) => {
+                registerContext.setEmail(e.target.value);
+              }}
+            ></input>
+            <input
+              className="input"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                registerContext.setPassword(e.target.value);
+              }}
+            ></input>
+            <input
+              className="input"
+              type="password"
+              placeholder="Confirm Password"
+              onChange={(e) => {
+                registerContext.setConfirmPassword(e.target.value);
+              }}
+            ></input>
+            <button
+              className="done-button"
+              disabled={registerContext.setfirstName}
+            >
+              Sign-Up
+            </button>
+          </form>
 
-      {registerContext.message && <div>{registerContext.message}</div>}
+          {registerContext.message && <div>{registerContext.message}</div>}
+        </div>
+      </div>
     </>
   );
 };
