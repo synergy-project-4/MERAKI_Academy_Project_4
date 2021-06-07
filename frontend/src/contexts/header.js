f478126f9bef23f6aa5c471a7132fa2928e3f586
TariqBazadough <tariq.bazadough@gmail.com>
2021-06-07 13:43:01 +0300

    fix conflict

--- frontend/src/contexts/header.js
+++ frontend/src/contexts/header.js
@@ -1,13 +1,12 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import ItemCardContext from './main'
+import ItemCardContext from "./main";
 export const HeaderContext = React.createContext();
 
 const HeaderProvider = (props) => {
-
-  
   const history = useHistory();
+  const [title, setTitle] = useState("");
   const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
   const [found, setFound] = useState([]);
@@ -22,21 +21,21 @@ const HeaderProvider = (props) => {
     filterItem,
   };
   async function searchItem() {
-
-
-    await axios.get('http://localhost:5000/main').then((result) => {
-      const find = result.data.filter((elem) => {
-        return elem.title == title || elem.tags == title
+    await axios
+      .get("http://localhost:5000/main")
+      .then((result) => {
+        const find = result.data.filter((elem) => {
+          return elem.title == title || elem.tags == title;
+        });
+        console.log(result);
+        setFound(find);
+        console.log("find:", find);
+        console.log("found ;;", found);
       })
-      console.log(result);
-      setFound(find)
-      console.log("find:", find);
-      console.log("found ;;", found)
-    }).
-      catch((error) => {
+      .catch((error) => {
         throw error;
-      })
-    history.push('/search/product');
+      });
+    history.push("/search/product");
   }
   async function filterItem(e) {
     try {
