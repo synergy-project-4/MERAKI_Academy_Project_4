f1091d9685f67accd8c16a4f15e4ebf724afa636
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-06-07 09:27:15 +0300

    search

--- frontend/src/components/searchProduct.js
+++ frontend/src/components/searchProduct.js
@@ -1,28 +1,23 @@
-import React, { useContext, useEffect,useState } from 'react';
-import { ItemCardContext } from './../../contexts/main';
-import axios from 'axios';
+import React ,{ useContext } from 'react';
 import { useHistory } from "react-router-dom";
-import './main.css'
-
-const Main = () => {
+import { ItemCardContext } from './../contexts/main';
+import "./main.css"
+const SearchProduct = (props) => {
 	const itemCardContext = useContext(ItemCardContext);
 	const history = useHistory();
-	useEffect(() => {
-		itemCardContext.showProduct();
-	}, []);
+    console.log("props",props)
 	const cardDetails = async (id) => {
 	
-		const foundItem = itemCardContext.products.find((elem) => {
+		const foundItem = props.item.find((elem) => {
 			return elem._id == id
 		})
 		itemCardContext.setFound(foundItem)
 		history.push('/product/details');
 	}
-
-	return (
-		<>
+    return (
+        <>
 			<div className="mainBody">
-				{itemCardContext.products.map((elem) => {
+				{props.item.map((elem) => {
 					return (
 						<div onClick={() => { cardDetails(elem._id) }} className="itemCard" key={elem._id}>
 							<p>{elem.title}</p>
@@ -35,7 +30,10 @@ const Main = () => {
 			<div></div>
 
 		</>
-	)
-};
+            
+        
+   
+    )
+}
 
-export default Main;
+export default SearchProduct;
--- frontend/src/contexts/header.js
+++ frontend/src/contexts/header.js
@@ -1,29 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
-
+import { useHistory } from "react-router-dom";
+import ItemCardContext from './main'
 export const HeaderContext = React.createContext();
 
 const HeaderProvider = (props) => {
-  const [search, setSearch] = useState("");
+  const history = useHistory();
+  const [products, setProducts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [found, setFound] = useState([]);
   const [message, setMessage] = useState("");
-
   const state = {
-    setSearch,
+    found,
+    setTitle,
+    products,
     message,
     searchItem,
-    filterItem
+    filterItem,
+    title,
   };
-
   async function searchItem() {
-    try {
-      await axios.post("http://localhost:5000/search/product",{
-        search
-      });
-    } catch (error) {
-      setMessage("item not found");
-    }
-  }
 
+    await axios.get('http://localhost:5000/main').then((result) => {
+      const find = result.data.filter((elem) => {
+        return elem.title == title || elem.tags == title
+      })
+      console.log(result);
+      setFound(find)
+      console.log("find:", find);
+      console.log("found ;;", found)
+    }).
+      catch((error) => {
+        throw error;
+      })
+    history.push('/search/product');
+  }
   async function filterItem(e) {
     console.log(e.target.value);
     try {
