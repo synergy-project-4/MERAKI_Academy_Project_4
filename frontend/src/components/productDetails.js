fd8328a3bcd9bb92ab86b6a356d9067d7c753dcb
TariqBazadough <tariq.bazadough@gmail.com>
2021-06-08 19:29:32 +0300

    done with CSS for the item details

--- frontend/src/components/productDetails.js
+++ frontend/src/components/productDetails.js
@@ -1,50 +1,50 @@
-import React, { useContext, useState } from 'react';
-import { LoginContext } from './../contexts/login';
-import './product.css'
-
+import React, { useContext, useState } from "react";
+import { LoginContext } from "./../contexts/login";
+import { useHistory } from "react-router-dom";
+import "./product.css";
 
 const ProductDetails = (props) => {
-    const loginContext = useContext(LoginContext);
-    const [notLogged, setNotLogged] = useState(false)
-    const [logged, setLogged] = useState(false)
+  const loginContext = useContext(LoginContext);
+  const [notLogged, setNotLogged] = useState(false);
+  const [logged, setLogged] = useState(false);
+  let history = useHistory();
 
-    const loginR = () => {
-        if (loginContext.token) {
-            setNotLogged(false)
-            setLogged(true)
-        } else {
-            {
-                setLogged(false)
-                setNotLogged(true)
-            }
-        }
-
-
+  const loginR = () => {
+    if (loginContext.token) {
+      setNotLogged(false);
+      setLogged(true);
+    } else {
+      {
+        setLogged(false);
+        setNotLogged(true);
+      }
     }
-    return (
-        <>
-            <div key={props.item._id}>
-                <p>{props.item.title}</p>
-                <p>{props.item.shortDescription}</p>
-                <p>{props.item.tags}</p>
-                <p>{props.item.price}</p>
-                <p>{props.item.description}</p>
-                <p>{props.item.quantity}</p>
-                <p>{props.item.location}</p>
-            </div>
-            <div>
-                <button onClick={loginR}>Add to cart</button>
-                {props.item.optionsToExchange && <button>Exchange</button>}
-
-            </div>
-            <div>
-                {notLogged ? <p className="failMessage">You must login</p>
-                    : ""}
-                {logged ? <p className="successMessage">Added to cart</p>
-                    : ""}
-            </div>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <div className="itemDetails">
+        <div key={props.item._id}>
+          <p className="title">
+            {props.item.title} ({props.item.shortDescription})
+          </p>
+          <p className="info">Tags : {props.item.tags}</p>
+          <p className="info">Description :{props.item.description}</p>
+          <p className="info">Located in: {props.item.location}</p>
+          <p className="price">In Stock : {props.item.quantity}</p>
+          <p className="price">price : {props.item.price}$</p>
+        </div>
+        <div className="add-button">
+          <button onClick={loginR}>Add to cart</button>
+          {props.item.optionsToExchange && <button>Exchange</button>}
+          <button onClick={history.goBack}>Go Back</button>
+        </div>
+        <div>
+          {notLogged ? <p className="failMessage">You must login</p> : ""}
+          {logged ? <p className="successMessage">Added to cart</p> : ""}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ProductDetails;
