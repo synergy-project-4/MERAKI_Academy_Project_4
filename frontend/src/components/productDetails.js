f3a96f77d73eaf434b95cb962fb96fcb4601e30a
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-08-18 12:57:34 +0300

    edit

--- frontend/src/components/productDetails.js
+++ frontend/src/components/productDetails.js
@@ -17,50 +17,6 @@ const ProductDetails = (props) => {
     const [pId, setPId] = useState();
     let history = useHistory();
 
-<<<<<<< HEAD
-    const loginAuth = async () => {
-        if (loginContext.token) {
-            setNotLogged(false);
-            setLogged(true);
-            itemCartContext.setQuantity(props.item.quantity);
-            cartContext.addToCart();
-        } else {
-            {
-                setLogged(false);
-                setNotLogged(true);
-            }
-        }
-    };
-    return (
-        <>
-            <div key={props.item._id} className="itemDetails">
-                
-                    <div><img className="product-img" src={props.item.image}></img></div>
-                    <div>
-                    <p className="title">
-                        {props.item.title} ({props.item.shortDescription})
-                    </p>
-                    
-                    <p className="info">Tags : {props.item.tags}</p>
-                    <p className="info">Description :{props.item.description}</p>
-                    <p className="info">Located in: {props.item.location}</p>
-                    <p className="price">In Stock : {props.item.quantity}</p>
-                    <p className="price">price : {props.item.price}$</p>
-                    <img src={backIcon} onClick={history.goBack} />
-                    </div>
-                
-                <div className="add-button">
-                    <img src={addToCart} onClick={loginAuth} /><br/>
-                    {props.item.optionsToExchange && <img src={exchangeIcon} />}<br/>
-                   
-               
-                    {notLogged ? <p className="failMessage">You must login</p> : ""}
-                    {logged ? <p className="successMessage">Added to cart</p> : ""}
-                </div>
-            </div>
-        </>
-    );
-=======
   const loginAuth = async () => {
     if (loginContext.token) {
       setNotLogged(false);
@@ -100,7 +56,6 @@ const ProductDetails = (props) => {
     </>
   );
 
->>>>>>> 9342bc070f8e5b296ebabd2b78eccf5440ab471d
 };
 
 export default ProductDetails;
--- frontend/src/components/searchProduct.js
+++ frontend/src/components/searchProduct.js
@@ -28,33 +28,37 @@ const SearchProduct = (props) => {
     itemCardContext.setFound(foundItem);
     history.push("/product/details");
   };
-
   return (
     <>
-      <div className="mainBody">
-        {props.item.map((elem) => {
-          if (elem.quantity !== 0) {
-            return (
-              <div
-                onClick={() => {
-                  cardDetails(elem._id);
-                }}
-                className="itemCard"
-                key={elem._id}
-              >
-                <div>
-                  <img className="product-img" src={elem.image}></img>
+      {props.item.length !==0 ? (
+        <div className="mainBody">
+          {console.log(props)}
+          {props.item.map((elem) => {
+            if (elem.quantity !== 0) {
+              return (
+                <div
+                  onClick={() => {
+                    cardDetails(elem._id);
+                  }}
+                  className="itemCard"
+                  key={elem._id}
+                >
+                  <div>
+                    <img className="product-img" src={elem.image}></img>
+                  </div>
+                  <div>
+                    <p className="title">{elem.title}</p>
+                    <p className="info">{elem.shortDescription}</p>
+                    <p className="info">Located in: {elem.location}</p>
+                    <p className="price">In Stock : {elem.quantity}</p>
+                    <p className="price">price : {elem.price}$</p>
+                  </div>
                 </div>
-                <div>
-                  <p className="title">{elem.title}</p>
-                  <p className="info">{elem.shortDescription}</p>
-                  <p className="price">{elem.price}</p>
-                </div>
-              </div>
-            );
-          }
-        })}
-      </div>
+              );
+            }
+          })}
+        </div>) : (<h2>there no items to display at the moment</h2>
+      )}
       <Pagination />
       <div></div>
     </>
