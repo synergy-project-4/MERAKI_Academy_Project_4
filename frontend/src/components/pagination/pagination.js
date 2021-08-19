8af67a16cb1b303b8aa9f46d7baa7d069a92e48c
TariqBazadough <tariq.bazadough@gmail.com>
2021-08-19 13:34:04 +0300

    done with V1 desgin refactor by creating css variables

--- frontend/src/components/pagination/pagination.js
+++ frontend/src/components/pagination/pagination.js
@@ -4,33 +4,33 @@ import { ItemCardContext } from "./../../contexts/main";
 import { HeaderContext } from "../../contexts/header";
 
 const Pagination = () => {
-    const itemCardContext = useContext(ItemCardContext);
-    const headerContext = useContext(HeaderContext);
+  const itemCardContext = useContext(ItemCardContext);
+  const headerContext = useContext(HeaderContext);
 
-    const handlePageClick = (e) => {
-        const selectedPage = e.selected;
-        itemCardContext.setOffset(selectedPage * headerContext.page);
-    };
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    itemCardContext.setOffset(selectedPage * itemCardContext.perPage);
+  };
 
-    return (
-        <>
-            <div className="paginate-page">
-                <ReactPaginate
-                    previousLabel={"prev"}
-                    nextLabel={"next"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={headerContext.page}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={"pagination"}
-                    subContainerClassName={"pages pagination"}
-                    activeClassName={"active"}
-                />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="paginate-page">
+        <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={headerContext.page}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Pagination;
