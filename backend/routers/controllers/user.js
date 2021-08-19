71278f1d15e6d1a0089c74c1f555df6ed0ba1357
arefallabadi <labadiaref@gmail.com>
2021-08-19 13:31:54 +0300

    edite functionlity for editProfile

--- backend/routers/controllers/user.js
+++ backend/routers/controllers/user.js
@@ -1,51 +1,51 @@
 const usersModel = require("./../../db/models/user");
+const bcrypt = require("bcrypt");
+const salt = Number(process.env.SALT);
 ///////////////////////////////////////////////////////////
 //edit profile
-const editProfile = (req, res) => {
-    const id = req.query.id;
-    const { password, confirmPassword } = req.body;
-    if (password == confirmPassword) {
-        usersModel
-            .findByIdAndUpdate(id, req.body, { new: true })
-            .then((result) => {
-                res.status(200).json(result);
-            })
-            .catch((err) => {
-                res.send(err);
-            });
-    }
-    else {
-         res.send("password not equal confirmPassword");
-         }
-}
+const editProfile = async (req, res) => {
+  const id = req.query.id;
+  let password = await bcrypt.hash(req.body.password, salt);
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let city = req.body.city;
+  usersModel
+    .findByIdAndUpdate(id, {firstName, lastName, city, password},{ new: true })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.send("can't edit profile");
+    });
+};
 ///////////////////////////////////////////////////////////
 //delete profile
 const deleteProfile = (req, res) => {
-    const id = req.query.id;
-    usersModel
-        .findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ message: "success delete the user" });
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-};
-
-const getUserById = (req,res)=>{
-    const id = req.query.id;
-    usersModel
-    .findById(id)
+  const id = req.query.id;
+  usersModel
+    .findByIdAndDelete(id)
     .then((result) => {
-        res.json(result);
+      res.json({ message: "success delete the user" });
     })
     .catch((err) => {
-        res.send(err);
+      res.send(err);
     });
-}
+};
+
+const getUserById = (req, res) => {
+  const id = req.query.id;
+  usersModel
+    .findById(id)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
 module.exports = {
-    editProfile,
-    deleteProfile,
-    getUserById
-}
+  editProfile,
+  deleteProfile,
+  getUserById,
+};
